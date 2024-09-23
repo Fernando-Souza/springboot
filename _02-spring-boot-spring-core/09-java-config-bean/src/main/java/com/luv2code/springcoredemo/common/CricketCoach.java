50aa966f6ac8312548639bdf6479cb49c105d33d
Fernando Souza <fernandoantonio.souza@outlook.com>
2024-09-23 18:32:53 -0300

    remocao de imports não utilizados

--- _02-spring-boot-spring-core/09-java-config-bean/src/main/java/com/luv2code/springcoredemo/common/CricketCoach.java
+++ _02-spring-boot-spring-core/09-java-config-bean/src/main/java/com/luv2code/springcoredemo/common/CricketCoach.java
@@ -1,7 +1,5 @@
 package com.luv2code.springcoredemo.common;
 
-import jakarta.annotation.PostConstruct;
-import jakarta.annotation.PreDestroy;
 import org.springframework.stereotype.Component;
 
 @Component
